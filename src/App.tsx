99528f8b5783093ae809a50b5678c9a9803ee8f8
Stefan Mazare <stefanmazare@fsociety.local>
2025-05-08 14:48:57 +0300

    added the cat and ... yeah

---
+++ src/App.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+import { OrbitControls, Stage } from "@react-three/drei";
+import "./App.css";
+import { Canvas } from "@react-three/fiber";
+import * as THREE from "three";
+import NekoCat from "./components/neko_cat/NekoCat";
+
+function App() {
+  const material = new THREE.MeshStandardMaterial({
+    color: "magenta",
+    emissive: "black",
+    emissiveIntensity: 0,
+    metalness: 0.5,
+    roughness: 0.5
+  });
+
+  return (
+    <Stage
+      environment="city"
+      intensity={0.5}
+      shadows={false}
+      adjustCamera={true}
+      preset="rembrandt"
+    >
+      <NekoCat
+        scale={0.5}
+        position={[0, 0, 0]}
+        rotation={[0, Math.PI / 2, 0]}
+        material={material}
+      />
+
+      <ambientLight intensity={1} />
+      <pointLight position={[0, 7, 1]} intensity={400} />
+    </Stage>
+  );
+}
+
+export default () => (
+  <Canvas>
+    <App />
+    <OrbitControls
+      enableZoom={true}
+      enablePan={false}
+      enableRotate={true}
+      autoRotate={true}
+      autoRotateSpeed={1}
+    />
+    <ambientLight intensity={1} />
+    <pointLight position={[0, 7, 1]} intensity={400} />
+  </Canvas>
+);
