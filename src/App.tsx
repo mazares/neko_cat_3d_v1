283583a61aad26b0c0e82ea666f8932099bbed77
Stefan Mazare <stefanmazare@fsociety.local>
2025-05-08 22:20:18 +0300

    simple cat

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,17 @@
 import { OrbitControls, Stage } from "@react-three/drei";
 import "./App.css";
 import { Canvas } from "@react-three/fiber";
-import * as THREE from "three";
+// import * as THREE from "three";
 import NekoCat from "./components/neko_cat/NekoCat";
 
 function App() {
-  const material = new THREE.MeshStandardMaterial({
-    color: "magenta",
-    emissive: "black",
-    emissiveIntensity: 0,
-    metalness: 0.5,
-    roughness: 0.5
-  });
+  // const material = new THREE.MeshStandardMaterial({
+  //   color: "magenta",
+  //   emissive: "black",
+  //   emissiveIntensity: 0,
+  //   metalness: 0.5,
+  //   roughness: 0.5
+  // });
 
   return (
     <Stage
@@ -27,32 +27,6 @@ function App() {
 
       <ambientLight intensity={1} />
       <pointLight position={[0, 7, 1]} intensity={400} />
-
-      <mesh
-        castShadow
-        receiveShadow
-        geometry={new THREE.BoxGeometry(1, 1, 1)}
-        material={material}
-        position={[0, 1, 0]}
-        rotation={[0, 0, 0]}
-        scale={[2, 1, 2]}
-      >
-        <cylinderGeometry args={[1, 1, 1, 32]} />
-        <meshStandardMaterial
-          color="green"
-          opacity={0.1}
-          transparent={true}
-          emissive="black"
-          emissiveIntensity={5}
-          metalness={0.5}
-          roughness={0.5}
-          side={THREE.DoubleSide}
-          depthWrite={false}
-          depthTest={false}
-          polygonOffset={true}
-          polygonOffsetFactor={-1}
-        />
-      </mesh>
     </Stage>
   );
 }
